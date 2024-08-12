74a0fba1deab0c244788040218b52c44ca768728
Isuru N Liyanaarachchi <isurunaveen27@gmail.com>
2024-08-12 16:22:18 +0530

    initial

---
+++ test_pyqt6.py
@@ -0,0 +1,13 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("PyQt6 Test Window")
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
